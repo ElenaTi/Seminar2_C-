1df940e3752cc562083b87cbec8305d452521242
Elena Timofeeva <posazho@gmail.com>
2022-06-27 23:41:19 +0300

    выкидываем вторую цифру. Нужно перепроверить

---
+++ Task11/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetRandomNumber(int minBorder, int maxBorder)
+{
+    return new Random().Next(minBorder, maxBorder + 1);
+}
+
+int result (int number)
+{
+    int NumberFirst = number / 100;
+    int NumberThird = number % 100;
+    int newNumber = (NumberFirst*10) + NumberThird;
+    return newNumber;
+}
+
+int randomNumber = GetRandomNumber(100, 999);
+int Digit = result(randomNumber);
+Console.WriteLine($"Случайное число {randomNumber} преобразуем в {Digit}");
+//Console.WriteLine($"Случайное число {randomNumber} преобразуем в {Digit}");
