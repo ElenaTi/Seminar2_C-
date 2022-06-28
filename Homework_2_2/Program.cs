a699c920880b99b72bd74994cc93ac53758ae22b
Elena Timofeeva <posazho@gmail.com>
2022-06-28 18:55:10 +0300

    Вывод третьей цифры числа, введенного пользователем

---
+++ Homework_2_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вывести третью цифру числа или ссобщить, что цифры нет
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetThirdDigit(int digit)
+{
+    int digitThree = digit % 10;
+    return digitThree;
+}
+
+if (number > 999)
+{
+    int number1 = number;
+    while (number1 > 999)
+    {
+       number1 = number1/10;
+    }
+    int ThreeDigitNumber = number1;
+    int digitThree1 = GetThirdDigit(ThreeDigitNumber);
+    Console.WriteLine($"В числе {number} третья цифра {digitThree1}.");
+}
+else if (number > 99 && number <1000)
+{
+    int digitThree = GetThirdDigit(number);
+    Console.WriteLine($"В числе {number} третья цифра {digitThree}.");
+}
+else
+{
+    Console.WriteLine($"В числе {number} третьей цифры нет.");
+}
