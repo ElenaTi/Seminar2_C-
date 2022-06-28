0a012ddbbeee1fc9f0056868df17336e08d08b3d
Elena Timofeeva <posazho@gmail.com>
2022-06-28 17:19:06 +0300

    Вывод информации о выходном дне

---
+++ Homework_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Принимает на вход цифру, обозначающую день недели. Проверяет, является ли выходным.
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number > 5 && number < 8)
+{
+    Console.WriteLine("Да, это выходной день.");
+}
+else if (number >0 && number < 6)
+{
+    Console.WriteLine("Нет, это рабочий день.");
+}
+else
+{
+    Console.WriteLine("Нет дня недели, соответствующего этому числу.");
+}
